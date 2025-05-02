595808ce498d6e1e454987cda7c72e1430e8a312
ritik6559 <ritikjoshi741@gmail.com>
2025-05-02 10:14:29 +0530

    minor changes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -23,6 +23,8 @@ const Admin = () => {
                 </p>
             </header>
 
+
+
             <main className={"admin-main"} >
                 <section className={"w-full space-y-4"} >
                     <h1 className={"header"} >
